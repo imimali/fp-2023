6386ecfd4445b0a07aebe127ad71129d96384d69
imimali <imimali18@gmail.com>
2024-01-16 09:21:07 +0200

    Add iterative backtracking + recursive with collector

--- seminar13/backtracking.py
+++ seminar13/backtracking.py
@@ -30,9 +30,52 @@ def back_recursive(elements: List[int], n: int, m: int):
         elements.pop()
 
 
+def back_recursive_with_collector(current_elements: List[int], n: int, m: int, solutions=None):
+    solutions = solutions if solutions is not None else []
+    if is_solution(current_elements, m):
+        solutions.append(current_elements[:])
+        return
+    for element in range(n):
+        current_elements.append(element)
+        if is_consistent(current_elements):
+            back_recursive_with_collector(current_elements, n, m, solutions)
+        current_elements.pop()
+
+
 def back_rec_wrapper(n, m):
     back_recursive([], n, m)
 
 
+def back_rec_collector_wrapper(n, m):
+    solutions = []
+    back_recursive_with_collector([], n, m, solutions)
+    return solutions
+
+
+def back_iterative(n: int, m: int):
+    solutions = []  # we collect the solutions here instead of simply printing them
+    stack = [[]]
+    while stack:
+        elements = stack.pop()
+        if is_solution(elements, m):
+            solutions.append(elements)
+            continue
+        # here we get through the possible values one by one.
+        # For example, for the problem with collinearity, you'd
+        # go through the list of points, for the problems with given
+        # number lists, you'd go through the list of numbers, etc.
+        # This is a more straightforward and flexible way than what
+        # that `x[-1] = x[-1] + 1` in the lecture did
+        for element in range(n):
+            new_elements = elements + [element]
+            # the + operator implicitly creates a new list, no
+            # need to copy new_elements again
+            if is_consistent(new_elements):
+                stack.append(new_elements)
+    return solutions
+
+
 if __name__ == '__main__':
-    back_rec_wrapper(3, 3)
+    print(back_rec_collector_wrapper(3, 3))
+    print(back_iterative(3, 3))
+    # find_lists(3, 3)
